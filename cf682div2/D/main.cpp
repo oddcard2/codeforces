d7f293944e58301701f6fbbae07c0c5ce536e2ba
iamoddcard <oddcard@yandex.ru>
2018-11-02 11:22:35 +0500

    358d2 D AC, nit refactored

--- cf682div2/D/main.cpp
+++ cf682div2/D/main.cpp
@@ -10,12 +10,18 @@ string input = "9 12 4\n"\
 //"aabd\n"\
 //"aacd\n";
 
+//string input = "5 5 4\n"\
+//"aaaaf\n"\
+//"aaaaa\n";
+
+
 struct State
 {
 	int p1;
 	int p2;
 	//int maxp1;
 	//int maxp2;
+	int part_len = 0;
 	int len = 0;
 };
 
@@ -50,8 +56,8 @@ int main(int argc, char **argv) {
 	std::getline(icp, s1);
 	std::getline(icp, s2);
 
-	cout << s1 << endl;
-	cout << s2 << endl;
+	//cout << s1 << endl;
+	//cout << s2 << endl;
 
 	for (int i = 0; i < n; i++)
 	{
@@ -73,24 +79,30 @@ int main(int argc, char **argv) {
 
 				//only this part
 				dp[i + 1][j + 1][1].len = len;
+				dp[i + 1][j + 1][1].part_len = len;
 				dp[i + 1][j + 1][1].p1 = i + 1;
 				dp[i + 1][j + 1][1].p2 = j + 1;
 
-				for (int l = 1; l <= len; l++)
-				{
-					if ((i + 1 - l < 0) || (j + 1 - l < 0))
-						break;
-
-					//num of parts
-					for (int k = 2; k <= kk; k++) {
-						if (k > 0 && dp[i + 1 - l][j + 1 - l][k - 1].len == 0) //no slitting before for k-1 parts
-							break;
-
-						//take l last symbols as last part
-						dp[i + 1][j + 1][k].len = dp[i + 1 - l][j + 1 - l][k - 1].len + l;
-						dp[i + 1][j + 1][k].p1 = i + 1;
-						dp[i + 1][j + 1][k].p2 = j + 1;
+				// num of parts
+				for (int k = 2; k <= kk; k++) {
+					int check_len = 1;
+					int exists = dp[i][j][k].len != 0;
+					if (exists) {
+						int part_used = len > 1 && dp[i][j][k].p1 == i && dp[i][j][k].p2 == j;
+						if (part_used) {
+							check_len = dp[i][j][k].part_len + 1;
+						}
+						else {
+							check_len = len;
+						}
 					}
+					//take check_len last symbols as last part
+					if (dp[i + 1 - check_len][j + 1 - check_len][k - 1].len == 0) //no slitting before for k-1 parts
+						continue;
+					dp[i + 1][j + 1][k].len = dp[i + 1 - check_len][j + 1 - check_len][k - 1].len + check_len;
+					dp[i + 1][j + 1][k].part_len = check_len;
+					dp[i + 1][j + 1][k].p1 = i + 1;
+					dp[i + 1][j + 1][k].p2 = j + 1;
 				}
 			}
 			//updates values
@@ -105,7 +117,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	cout << dp[n][m][kk].len << " " << dp[n][m][kk].p1 << " " << dp[n][m][kk].p2 << endl;
+	cout << dp[n][m][kk].len/* << " " << dp[n][m][kk].p1 << " " << dp[n][m][kk].p2 */<< endl;
 
 	return 0;
 }
