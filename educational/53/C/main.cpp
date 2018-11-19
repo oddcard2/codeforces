3837a7ac16e4dc715e5861093f410ec45a7f38dc
iamoddcard <oddcard@yandex.ru>
2018-11-19 16:48:09 +0500

    ed53 C AC

--- educational/53/C/main.cpp
+++ educational/53/C/main.cpp
@@ -1,29 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "bits/stdc++.h"
 
 using namespace std;
 
-string input = "";
+typedef int64_t i64;
 
-int main(int argc, char **argv) {
-	cout.sync_with_stdio(false);
-	cin.sync_with_stdio(false);
+#define pri(x) printf("%d", (x));
+#define sci(x) scanf("%d\n", &(x));
+#define sci2(x, y) scanf("%d %d", &(x), &(y));
 
-	istream *pinp;
-#ifndef ONLINE_JUDGE
-	unique_ptr<istream> stream;
-	if (argc > 1) {
-		pinp = new ifstream(argv[1]);
-		stream.reset(pinp);
+char line[200005];
+
+int main() {
+	int n,x,y;
+
+	scanf("%d %s %d %d", &n, line, &x, &y);
+
+	auto ok = [&](int l) {
+		int x1 = 0, y1 = 0;
+		for (int i = l; i < n; i++)
+		{
+			char d = line[i];
+			if (d == 'U')
+				y1++;
+			if (d == 'D')
+				y1--;
+			if (d == 'L')
+				x1--;
+			if (d == 'R')
+				x1++;
+		}
+
+		//int dd = (abs(x1 - x) + abs(y1 - y));
+		int dd = abs(x1 - x) + abs(y1 - y);
+		if (dd <= l && (dd % 2 == l % 2))
+			return true;
+		
+		for (int i = 0; i < n-l; i++)
+		{
+			char d = line[i+l];
+			if (d == 'U')
+				y1--;
+			if (d == 'D')
+				y1++;
+			if (d == 'L')
+				x1++;
+			if (d == 'R')
+				x1--;
+
+			d = line[i];
+			if (d == 'U')
+				y1++;
+			if (d == 'D')
+				y1--;
+			if (d == 'L')
+				x1--;
+			if (d == 'R')
+				x1++;
+
+			int dd = abs(x1 - x) + abs(y1 - y);
+			if (dd <= l && (dd % 2 == l % 2))
+				return true;
+		}
+
+		return false; 
+	};
+
+	if (!ok(n)) {
+		pri(-1);
+		return 0;
 	}
-	else if (!input.empty()) {
-		pinp = new istringstream(input);
-		stream.reset(pinp);
-	} else
-#endif	
-	{
-		pinp = &cin;
+
+	int l = -1, r = n, m;
+	while (r - l > 1) {
+		m = (l + r) / 2;
+		if (ok(m))
+			r = m;
+		else
+			l = m;
 	}
-	istream &icp = *pinp;
-	
+
+	pri(r);
 	return 0;
 }
