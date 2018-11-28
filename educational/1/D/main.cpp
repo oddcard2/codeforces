a40fe0a1e65d5889b4e9b6966384a5174f95e23f
oddcard_hp <oddcard@yandex.ru>
2018-11-28 11:38:04 +0500

    ed1 d ac

--- educational/1/D/main.cpp
+++ educational/1/D/main.cpp
@@ -8,6 +8,15 @@ typedef long long ll;
 
 #define MAX_VAL 1000000000ll
 
+struct node {
+	char type; //0 - ., 1- *
+	char sum;
+	char used;
+	int comp;
+};
+
+node field[1001][1001];
+
 
 int main(int argc, char **argv) {
 	ios::sync_with_stdio(false);
@@ -19,111 +28,60 @@ int main(int argc, char **argv) {
 	string line;
 	getline(cin, line);
 
-	struct node {
-		int sum;
-		int comp;
-	};
-
-	vector<vector<int> > num(n, vector<int>(m)); //(x,y) -> vectex num
+	vector<pair<int, int>> vs;
 	vector<int> comp_sizes;
 
-	vector<vector<int> > g;
-	vector<node> vs;
-
-	vector<string> l;
 	for (int i = 0; i < n; i++)
 	{
 		getline(cin, line);
 
-		l.push_back(line);
-	}
-
-	int id = 0;
-	for (int i = 0; i < n; i++)
-	{
 		for (int j = 0; j < m; j++) {
-			char c = l[i][j];
-			if (c == '.') { //vertex!
-				num[i][j] = id++;
-			}
-			else {
-				num[i][j] = -1;
-			}
+			field[i][j].type = line[j] == '*';
+			if (!field[i][j].type)
+				vs.push_back({ i,j });
 		}
 	}
-	g.resize(id + 1);
-	vs.resize(id + 1);
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++) {
-			char c = l[i][j];
-			if (c == '.') { //vertex!
-				id = num[i][j];
-				int s = 0;
-				if (i > 0) {
-					if (l[i - 1][j] == '*')
-						s++;
-					if (l[i - 1][j] == '.')
-						g[id].push_back(num[i - 1][j]);
-				}
-				if (i < n - 1) {
-					if (l[i +1][j] == '*')
-						s++;
-					if (l[i + 1][j] == '.')
-						g[id].push_back(num[i + 1][j]);
-				}
-				if (j > 0) {
-					if (l[i][j-1] == '*')
-						s++;
-					if (l[i][j - 1] == '.')
-						g[id].push_back(num[i][j-1]);
-				}
-				if (j < m - 1) {
-					if (l[i][j+1] == '*')
-						s++;
-					if (l[i][j + 1] == '.')
-						g[id].push_back(num[i][j + 1]);
-				}
-
-				vs[id].sum = s;
-			}
-		}
-	}
-
-	n = id + 1;
 
 	int c = 0;
-	vector<bool> used(n);
+	auto calc = [&](int i, int j) {
+		int s = 0;
+		if (field[i - 1][j].type) s++;
+		if (field[i + 1][j].type) s++;
+		if (field[i][j-1].type) s++;
+		if (field[i][j+1].type) s++;
+		return s;
+	};
+	
+	n = (int)vs.size();
 	for (int s = 0; s < n; s++)
 	{
-		if (used[s])
+		if (field[vs[s].first][vs[s].second].used)
 			continue;
 
-		int comp_size = vs[s].sum;
-		queue<int> q;
-		q.push(s);
+		int comp_size = calc(vs[s].first, vs[s].second);
+		queue<pair<int,int>> q;
+		q.push(vs[s]);
 
-		vector<int> d(n), p(n);
-		used[s] = true;
-		vs[s].comp = c;
+		field[vs[s].first][vs[s].second].used = 1;
+		field[vs[s].first][vs[s].second].comp = c;
 
-		p[s] = -1;
+#define PROCESS(i,j)\
+		if (field[i][j].type == 0 && !field[i][j].used) {\
+			field[i][j].used = 1;\
+			field[i][j].comp = c;\
+			comp_size += calc(i, j);\
+			q.push({ i,j });\
+		}
+
 		while (!q.empty()) {
-			int v = q.front();
+			int i, j;
+			tie(i,j) = q.front();
 			q.pop();
-			for (size_t i = 0; i < g[v].size(); ++i) {
-				int to = g[v][i];
-				if (!used[to]) {
-					used[to] = true;
-					vs[to].comp = c;
-					comp_size += vs[to].sum;
 
-					q.push(to);
-					d[to] = d[v] + 1;
-					p[to] = v;
-				}
-			}
+			PROCESS((i - 1), j);
+			PROCESS((i + 1), j);
+			PROCESS((i), (j-1));
+			PROCESS((i), (j+1));
 		}
 		comp_sizes.push_back(comp_size);
 		c++;
@@ -131,19 +89,16 @@ int main(int argc, char **argv) {
 	
 	for (int i = 0; i < k; i++)
 	{
-		if (k < 50000) {
-			int x, y;
-			cin >> x >> y;
-			x--;
-			y--;
-			int nn = num[x][y];
-			if (nn < 0)
-				cout << "0" << '\n';
-			else
-				cout << comp_sizes[vs[nn].comp] << '\n';
-		}
-		else {
+		int x, y;
+		cin >> x >> y;
+		x--;
+		y--;
+
+		if (field[x][y].type)
 			cout << "0" << '\n';
+		else {
+			int comp = field[x][y].comp;
+			cout << comp_sizes[comp] << '\n';
 		}
 	}
 
