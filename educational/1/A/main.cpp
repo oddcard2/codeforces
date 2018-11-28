bed210dbf7b0c47bc96872c4e098e0ef9efb1a62
oddcard_hp <oddcard@yandex.ru>
2018-11-28 12:56:54 +0500

    ed1 a and b ac

--- educational/1/A/main.cpp
+++ educational/1/A/main.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int main(int argc, char **argv) {
-	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+
+	vector<ll> a(t);
+
+	for (int i = 0; i < t; i++)
+	{
+		cin >> a[i];
+	}
+
+	vector<ll> pows;
+	pows.push_back(1);
+	ll v = 1;
+	while (v <= 1000000000ll) {
+		v *= 2;
+		pows.push_back(v);
+	}
+
+	auto less_p = [&](ll v) {
+		for (size_t i = 0; i < pows.size(); ++i) {
+			if (pows[i] > v) {
+				return pows[i - 1];
+			}
+		}
+	};
+
+	for (int i = 0; i < t; i++) {
+		ll v = a[i];
+		ll ans = v * (v + 1) / 2 - 2*(less_p(v) * 2 - 1);
+		cout << ans << '\n';
+	}
+
 	return 0;
 }
--- educational/1/B/main.cpp
+++ educational/1/B/main.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
-	
+typedef long long ll;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string line;
+	getline(cin, line);
+
+
+	string tmp(line);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int l, r, k;
+		cin >> l >> r >> k;
+		l--; r--;
+
+		int len = r - l + 1;
+		int npos = (k%len);
+
+		for (int j = 0; j < len; j++)
+		{
+			tmp[(npos + j) % len] = line[l + j];
+		}
+		for (int j = 0; j < len; j++) {
+			line[l + j] = tmp[j];
+		}
+	}
+	cout << line << '\n';
 	return 0;
 }
