7fd338dff8e16b69c46998fe5d424c8a03c39efb
oddcard_hp <oddcard@yandex.ru>
2018-11-29 15:58:59 +0500

    ed2 C refactoring

--- educational/2/C/main.cpp
+++ educational/2/C/main.cpp
@@ -10,70 +10,38 @@ int main() {
 	getline(cin, s);
 
 	int stat[26] = { 0 };
-
 	for (auto c : s) {
 		stat[c - 'a']++;
 	}
 
-	int cnt = 0;
+	deque<int> q;
 	for (int i = 0; i < 26; i++)
-	{
-		if (stat[i] % 2) {
-			cnt++;
-		}
-	}
-	if (cnt > 1) {
-		//changes
-		int i = 0, j = 25;
-		while (j > i) {
-			for (; i < 26; i++) {
-				if (stat[i] % 2) {
-					break;
-				}
-			}
-			for (; j >= 0; j--) {
-				if (stat[j] % 2) {
-					break;
-				}
-			}
-			if (i < 26 && j >= 0) {
-				stat[i]++;
-				stat[j]--;
-			}
-		}
-	}
-	//output
-	int center = -1;
-	for (int i = 0; i < 26; i++)
-	{
-		if (stat[i] % 2) {
-			center = i;
-			break;
-		}
+		if (stat[i] % 2)
+			q.push_back(i);
+
+	while (q.size() >= 2) {
+		stat[q.front()]++;
+		stat[q.back()]--;
+		q.pop_back();
+		q.pop_front();
 	}
 
+	string out(s);
+	int n = s.size();
+	int p = 0;
 	for (int i = 0; i < 26; i++) {
-		if (!stat[i])
-			continue;
-
-		int num = stat[i] / 2;
-		for (int j = 0; j < num; j++) {
-			cout << (char)(i + 'a');
+		while (stat[i] > 1) {
+			out[p] = out[n-p-1] = (char)(i + 'a');
+			p++;
+			stat[i] -= 2;
 		}
-		//stat[i] -= num;
 	}
-	if (center >= 0) {
-		cout << (char)(center + 'a');
+	for (int i = 0; i < 26; i++) {
+		if (stat[i]) {
+			out[n/2]= (char)(i + 'a');
+		}
 	}
-
-	for (int i = 25; i >=0; i--) {
-		if (!stat[i])
-			continue;
-
-		int num = stat[i] / 2;
-		for (int j = 0; j < num; j++)
-			cout << char(i + 'a');
-	}
+	cout << out;
 
 	return 0;
 }
