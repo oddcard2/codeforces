615ebeb954e348bb6ae8ed65600ddf4431b1edb6
oddcard_hp <oddcard@yandex.ru>
2018-11-29 15:00:29 +0500

    ed2 B AC

--- educational/2/B/main.cpp
+++ educational/2/B/main.cpp
@@ -2,7 +2,50 @@
 
 using namespace std;
 
-int main(int argc, char **argv) {
-	
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> v1(n);
+
+	struct node
+	{
+		int val;
+		int num;
+		int cnt;
+	};
+	vector<node> v2(m);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v1[i];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cin >> v2[i].val;
+		v2[i].num = i;
+	}
+
+	sort(begin(v1), end(v1));
+
+	sort(begin(v2), end(v2), [](node& a, node& b) { return a.val < b.val; });
+
+	int cnt = 0;
+	int p2 = 0;
+	for (int i = 0; i < m; i++) {
+		while (p2 < n && v1[p2] <= v2[i].val) {
+			cnt++;
+			p2++;
+		}
+		v2[i].cnt = cnt;
+	}
+
+	sort(begin(v2), end(v2), [](node& a, node& b) { return a.num < b.num; });
+	for (int i = 0; i < m; i++) {
+		cout << v2[i].cnt << " ";
+	}
 	return 0;
 }
