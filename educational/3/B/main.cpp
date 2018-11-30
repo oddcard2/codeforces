b8745c79ff3a8828e137338a2f8eeeb126ddf70b
oddcard_hp <oddcard@yandex.ru>
2018-11-30 15:56:46 +0500

    ed3 B AC

--- educational/3/B/main.cpp
+++ educational/3/B/main.cpp
@@ -8,5 +8,29 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
+	int n, m;
+
+	cin >> n >> m;
+
+	vector<int> a(m);
+	for (int i = 0; i < n; i++)
+	{
+		int b;
+		cin >> b;
+
+		a[b - 1]++;
+	}
+
+	ll ans = 0;
+	for (int i = 0; i < m-1; i++)
+	{
+		for (int j = i+1; j < m; j++)
+		{
+			ans += a[i] * a[j];
+		}
+	}
+
+	cout << ans;
+
 	return 0;
 }
