2fcae914af3fbd41b1e27ac4637fb11216bf4b9c
oddcard_hp <oddcard@yandex.ru>
2018-12-23 23:17:40 +0500

    Ed6 D AC

--- educational/6/D/main.cpp
+++ educational/6/D/main.cpp
@@ -47,11 +47,71 @@ int main() {
 	}
 
 	if (a.size() > 1 && b.size() > 1) {
-
-		map<ll, pair<int, int>> ma;
+		struct V {
+			ll v;
+			int a, b;
+		};
+		
+#if 1
+		vector<V> ma;
 		for (size_t i = 0; i < a.size(); i++)
 		{
 			for (size_t j = i+1; j < a.size(); j++)
+			{
+				ma.push_back({a[i] + a[j], (int)i, (int)j });
+			}
+		}
+
+		vector<V> mb;
+		for (size_t i = 0; i < b.size(); i++)
+		{
+			for (size_t j = i+1; j < b.size(); j++)
+			{
+				mb.push_back({ b[i] + b[j], (int)i, (int)j });
+			}
+		}
+
+		sort(begin(ma), end(ma), [](V& v1, V& v2) { return v1.v < v2.v; });
+		sort(begin(mb), end(mb), [](V& v1, V& v2) { return v1.v < v2.v; });
+
+		auto ma_end = unique(begin(ma), end(ma), [](V& v1, V& v2) { return v1.v == v2.v; });
+		auto mb_end = unique(begin(mb), end(mb), [](V& v1, V& v2) { return v1.v == v2.v; });
+
+		for (auto v = ma.begin(); v != ma_end; ++v)
+		{
+			ll s = suma - v->v;
+			ll req = (sumb + v->v - s) / 2;
+
+			auto lb = lower_bound(begin(mb), mb_end, V{ req,0,0 }, [](const V& v1, const V& v2) { return v1.v < v2.v; });
+			if (lb != mb.end()) {
+				ll val = abs((suma - v->v + lb->v) - (sumb - lb->v + v->v));
+				if (val < best) {
+					na1 = v->a;
+					na2 = v->b;
+					nb1 = lb->a;
+					nb2 = lb->b;
+					best = val;
+					k = 2;
+				}
+			}
+			if (lb != mb.begin()) {
+				auto lbb = --lb;
+				ll val = abs((suma - v->v + lbb->v) - (sumb - lbb->v + v->v));
+				if (val < best) {
+					na1 = v->a;
+					na2 = v->b;
+					nb1 = lbb->a;
+					nb2 = lbb->b;
+					best = val;
+					k = 2;
+				}
+			}
+		}
+#else 
+		map<ll, pair<int, int>> ma;
+		for (size_t i = 0; i < a.size(); i++)
+		{
+			for (size_t j = i + 1; j < a.size(); j++)
 			{
 				ma[a[i] + a[j]] = { i,j };
 			}
@@ -60,13 +120,13 @@ int main() {
 		map<ll, pair<int, int>> mb;
 		for (size_t i = 0; i < b.size(); i++)
 		{
-			for (size_t j = i+1; j < b.size(); j++)
+			for (size_t j = i + 1; j < b.size(); j++)
 			{
 				mb[b[i] + b[j]] = { i,j };
 			}
 		}
 
-		for (auto& v: ma)
+		for (auto& v : ma)
 		{
 			ll s = suma - v.first;
 			ll req = (sumb + v.first - s) / 2;
@@ -96,6 +156,7 @@ int main() {
 				}
 			}
 		}
+#endif
 	}
 
 	cout << best << '\n';
