51a8f98f997712df8ccc167c00031fd9ad1d5ef8
iamoddcard <oddcard@yandex.ru>
2019-10-14 12:44:17 +0500

    debug added to template

--- SolutionGenVs17/template/Project/main.cpp
+++ SolutionGenVs17/template/Project/main.cpp
@@ -63,6 +63,37 @@ int n; cin >> n;\
 vi v(n+1); \
 for (int i = 0; i < n; i++) cin >> v[i+1];
 
+/// DEBUG
+/// https://codeforces.com/blog/entry/68809
+/// TODO: compare with https://codeforces.com/blog/entry/65311
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) { cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'; }
+template<typename T>
+void __print(const T &x) { int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}"; }
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v) { __print(t); if (sizeof...(v)) cerr << ", "; _print(v...); }
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
 ////////////
 
 int main() {
